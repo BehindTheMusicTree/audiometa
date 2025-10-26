3293e80148546769ae79e71def263f2328c12482
mignot <andreas.garcia@bodzify.com>
2025-10-26 07:11:26 +0100

    test: simplify Vorbis publisher writing test to verify metadata retrieval

--- audiometa/test/tests/integration/specific_field/publisher/test_writing.py
+++ audiometa/test/tests/integration/specific_field/publisher/test_writing.py
@@ -28,15 +28,12 @@ class TestPublisherWriting:
                 update_metadata(test_file.path, test_metadata, metadata_format=MetadataFormat.RIFF)
 
     def test_vorbis(self):
-        from audiometa.exceptions import MetadataFieldNotSupportedByMetadataFormatError
-        
         with TempFileWithMetadata({}, "flac") as test_file:
             test_publisher = "Test Publisher Vorbis"
             test_metadata = {UnifiedMetadataKey.PUBLISHER: test_publisher}
-        
-            # Vorbis format raises exception for unsupported metadata
-            with pytest.raises(MetadataFieldNotSupportedByMetadataFormatError, match="UnifiedMetadataKey.PUBLISHER metadata not supported by this format"):
-                update_metadata(test_file.path, test_metadata, metadata_format=MetadataFormat.VORBIS)
+            update_metadata(test_file.path, test_metadata, metadata_format=MetadataFormat.VORBIS)
+            publisher = get_unified_metadata_field(test_file.path, UnifiedMetadataKey.PUBLISHER)
+            assert publisher == test_publisher
 
     def test_invalid_type_raises(self):
         from audiometa.exceptions import InvalidMetadataFieldTypeError
