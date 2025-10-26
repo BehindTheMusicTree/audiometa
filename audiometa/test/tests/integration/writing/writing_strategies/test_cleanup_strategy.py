d1aeeb15e1e3f465ce6975c254028ad462aab830
mignot <andreas.garcia@bodzify.com>
2025-10-26 10:08:32 +0100

    test: refactor cleanup strategy tests to use ID3 metadata setters for improved clarity

--- audiometa/test/tests/integration/writing/writing_strategies/test_cleanup_strategy.py
+++ audiometa/test/tests/integration/writing/writing_strategies/test_cleanup_strategy.py
@@ -11,21 +11,27 @@ from audiometa import (
     update_metadata,
     get_unified_metadata,
 )
-from audiometa.test.helpers.id3v2.id3v2_metadata_setter import ID3v2MetadataSetter
 from audiometa.utils.MetadataFormat import MetadataFormat
 from audiometa.utils.MetadataWritingStrategy import MetadataWritingStrategy
 from audiometa.utils.UnifiedMetadataKey import UnifiedMetadataKey
 from audiometa.test.helpers.temp_file_with_metadata import TempFileWithMetadata
+from audiometa.test.helpers.id3v2.id3v2_metadata_setter import ID3v2MetadataSetter
+from audiometa.test.helpers.id3v1.id3v1_metadata_setter import ID3v1MetadataSetter
 
 
 @pytest.mark.integration
 class TestCleanupStrategy:
 
-    def test_cleanup_strategy_wav_with_id3v2(self):
+    def test_cleanup_strategy_wav(self):
         with TempFileWithMetadata({"title": "Basic Title", "artist": "Basic Artist", "album": "Basic Album"}, "wav") as test_file:
             ID3v2MetadataSetter.set_metadata(test_file.path, {"title": "ID3v2 Title", "artist": "ID3v2 Artist", "album": "ID3v2 Album"}, version='2.3')
             
-            update_metadata(test_file.path, {"title": "RIFF Title", "artist": "RIFF Artist", "album": "RIFF Album"}, metadata_strategy=MetadataWritingStrategy.CLEANUP, metadata_format=MetadataFormat.RIFF)
+            metadata = {
+                UnifiedMetadataKey.TITLE: "RIFF Title",
+                UnifiedMetadataKey.ARTISTS: ["RIFF Artist"],
+                UnifiedMetadataKey.ALBUM: "RIFF Album"
+            }
+            update_metadata(test_file.path, metadata, metadata_strategy=MetadataWritingStrategy.CLEANUP)
             
             # Verify ID3v2 was removed
             id3v2_after = get_unified_metadata(test_file.path, metadata_format=MetadataFormat.ID3V2)
@@ -43,9 +49,7 @@ class TestCleanupStrategy:
         # Create test file with ID3v1 metadata using external script
         with TempFileWithMetadata({}, "mp3") as test_file:
             # Add ID3v1 metadata using TempFileWithMetadata
-            test_file.set_id3v1_title("ID3v1 Title")
-            test_file.set_id3v1_artist("ID3v1 Artist")
-            test_file.set_id3v1_album("ID3v1 Album")
+            ID3v1MetadataSetter.set_metadata(test_file.path, {"title": "ID3v1 Title", "artist": "ID3v1 Artist", "album": "ID3v1 Album"})
             
             # Verify ID3v1 metadata was written
             id3v1_result = get_unified_metadata(test_file.path, metadata_format=MetadataFormat.ID3V1)
