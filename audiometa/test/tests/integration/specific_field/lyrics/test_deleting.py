c8914ad827b9c06c5b5dce4c409ee2e682917aa1
mignot <andreas.garcia@bodzify.com>
2025-10-26 09:31:03 +0100

    test: update ID3v2 metadata getter to specify version for improved accuracy in lyrics retrieval

--- audiometa/test/tests/integration/specific_field/lyrics/test_deleting.py
+++ audiometa/test/tests/integration/specific_field/lyrics/test_deleting.py
@@ -20,7 +20,7 @@ class TestLyricsDeleting:
     def test_delete_lyrics_id3v2_3(self):        
         with TempFileWithMetadata({}, "id3v2.3") as test_file:
             ID3v2MetadataSetter.set_lyrics(test_file.path, "Test lyrics")
-            raw_metadata = ID3v2MetadataGetter.get_raw_metadata(test_file.path)
+            raw_metadata = ID3v2MetadataGetter.get_raw_metadata(test_file.path, version='2.3')
             assert raw_metadata['USLT'] == ["eng\x00Test lyrics"]
         
             update_metadata(test_file.path, {UnifiedMetadataKey.UNSYNCHRONIZED_LYRICS: None}, metadata_format=MetadataFormat.ID3V2, id3v2_version=(2, 3, 0))
