b285ce0af12928dd247dfa778aac97f9a727f455
mignot <andreas.garcia@bodzify.com>
2025-10-26 09:34:52 +0100

    test: specify ID3v2 version when setting lyrics in test_delete_lyrics_id3v2_3

--- audiometa/test/tests/integration/specific_field/lyrics/test_deleting.py
+++ audiometa/test/tests/integration/specific_field/lyrics/test_deleting.py
@@ -19,7 +19,7 @@ class TestLyricsDeleting:
     
     def test_delete_lyrics_id3v2_3(self):        
         with TempFileWithMetadata({}, "id3v2.3") as test_file:
-            ID3v2MetadataSetter.set_lyrics(test_file.path, "Test lyrics")
+            ID3v2MetadataSetter.set_lyrics(test_file.path, "Test lyrics", version='2.3')
             raw_metadata = ID3v2MetadataGetter.get_raw_metadata(test_file.path, version='2.3')
             assert raw_metadata['USLT'] == ["eng\x00Test lyrics"]
         
