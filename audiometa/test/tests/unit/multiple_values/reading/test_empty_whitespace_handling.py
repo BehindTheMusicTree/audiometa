841f6faf723e254235fb033ea75467c25bb355a1
mignot <andreas.garcia@bodzify.com>
2025-10-26 18:32:40 +0100

    test: remove obsolete tests for single artist handling and empty metadata

--- audiometa/test/tests/unit/multiple_values/reading/test_empty_whitespace_handling.py
+++ audiometa/test/tests/unit/multiple_values/reading/test_empty_whitespace_handling.py
@@ -8,24 +8,6 @@ from audiometa.test.helpers.common.external_tool_runner import run_external_tool
 
 
 class TestEmptyWhitespaceHandling:
-    def test_no_multiple_entries_returns_single_value(self):
-
-        with TempFileWithMetadata({"title": "Test Song"}, "flac") as test_file:
-            VorbisMetadataSetter.set_artist(test_file.path, "Single Artist")
-            
-            artists = get_unified_metadata_field(test_file.path, UnifiedMetadataKey.ARTISTS, MetadataFormat.VORBIS)
-            
-            assert isinstance(artists, list)
-            assert len(artists) == 1
-            assert "Single Artist" in artists
-
-    def test_empty_metadata_returns_none(self):
-        with TempFileWithMetadata({"title": "Test Song"}, "flac") as test_file:
-            run_external_tool(["metaflac", "--remove-all-tags", str(test_file.path)], "metaflac")
-            
-            artists = get_unified_metadata_field(test_file.path, UnifiedMetadataKey.ARTISTS, MetadataFormat.VORBIS)
-            
-            assert artists is None
 
     def test_mixed_empty_and_valid_entries(self):
         with TempFileWithMetadata({"title": "Test Song"}, "flac") as test_file:
@@ -41,7 +23,7 @@ class TestEmptyWhitespaceHandling:
             assert "Valid Artist 3" in artists
             assert "" not in artists
 
-    def test_whitespace_only_entries(self):
+    def test_whitespace_entries(self):
         with TempFileWithMetadata({"title": "Test Song"}, "flac") as test_file:
             VorbisMetadataSetter.set_artists(test_file.path, ["Valid Artist", "   ", "\t", "\n", "Another Valid Artist"])
             
