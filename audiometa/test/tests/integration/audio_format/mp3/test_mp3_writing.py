0958b63afe4391a184074996df71ad97abb92801
mignot <andreas.garcia@bodzify.com>
2025-11-05 14:51:41 +0100

    refactor(test): refactor test_id3v1_metadata_writing_mp3 to use context manager for TempFileWithMetadata

--- audiometa/test/tests/integration/audio_format/mp3/test_mp3_writing.py
+++ audiometa/test/tests/integration/audio_format/mp3/test_mp3_writing.py
@@ -18,11 +18,12 @@ class TestMp3Writing:
             id3v2_title = ID3v2MetadataGetter.get_title(temp_mp3_file.path)
             assert id3v2_title == "Test Title Default"
 
-    def test_id3v1_metadata_writing_mp3(self, temp_audio_file):
-        metadata = {UnifiedMetadataKey.TITLE: "Test Title ID3v1"}
-        update_metadata(temp_audio_file, metadata, metadata_format=MetadataFormat.ID3V1)
-        read_metadata = get_unified_metadata(temp_audio_file, metadata_format=MetadataFormat.ID3V1)
-        assert read_metadata[UnifiedMetadataKey.TITLE] == "Test Title ID3v1"
+    def test_id3v1_metadata_writing_mp3(self):
+        with TempFileWithMetadata({}, "mp3") as temp_audio_file:
+            metadata = {UnifiedMetadataKey.TITLE: "Test Title ID3v1"}
+            update_metadata(temp_audio_file, metadata, metadata_format=MetadataFormat.ID3V1)
+            read_metadata = get_unified_metadata(temp_audio_file, metadata_format=MetadataFormat.ID3V1)
+            assert read_metadata[UnifiedMetadataKey.TITLE] == "Test Title ID3v1"
 
     def test_id3v2_3_metadata_writing_mp3(self):
         with TempFileWithMetadata({}, "id3v2.3") as test_file:
