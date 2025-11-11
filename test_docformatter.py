cc25c23a67a849e4f693631e240dc7b2ab71e054
mignot <andreas.garcia@bodzify.com>
2025-11-11 17:52:38 +0100

    test: docformatter behavior

---
+++ test_docformatter.py
@@ -0,0 +1,3 @@
+def test_function():
+    """This is a very long docstring that needs to be wrapped because it exceeds the maximum line length and should be formatted by docformatter to wrap it properly."""
+    pass
