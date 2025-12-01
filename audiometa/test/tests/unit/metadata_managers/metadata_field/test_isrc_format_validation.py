41d2243a32dd655c042a642fa92979f4a4f7cc59
mignot <garcia.andreas.1991@gmail.com>
2025-12-01 18:34:56 +0100

    refactor(tests): split ISRC validation tests into separate files
    - Create test_isrc_type_validation.py for type validation tests (6 tests)
    - Create test_isrc_format_validation.py for format validation tests (36 tests)
    - Remove combined test_isrc_validation.py file

--- audiometa/test/tests/unit/metadata_managers/metadata_field/test_isrc_format_validation.py
+++ audiometa/test/tests/unit/metadata_managers/metadata_field/test_isrc_format_validation.py
@@ -1,33 +1,10 @@
-"""Unit tests for ISRC metadata field type and format validation."""
+"""Unit tests for ISRC metadata field format validation."""
 
 import pytest
 
 from audiometa import validate_metadata_for_update
 from audiometa.exceptions import InvalidMetadataFieldFormatError, InvalidMetadataFieldTypeError
 from audiometa.utils.unified_metadata_key import UnifiedMetadataKey
-
-
-@pytest.mark.unit
-class TestISRCTypeValidation:
-    def test_valid_isrc_string(self):
-        validate_metadata_for_update({UnifiedMetadataKey.ISRC: "USRC17607839"})
-
-    def test_valid_isrc_with_hyphens(self):
-        validate_metadata_for_update({UnifiedMetadataKey.ISRC: "US-RC1-76-07839"})
-
-    def test_invalid_isrc_type_integer_raises(self):
-        with pytest.raises(InvalidMetadataFieldTypeError):
-            validate_metadata_for_update({UnifiedMetadataKey.ISRC: 12345})
-
-    def test_invalid_isrc_type_list_raises(self):
-        with pytest.raises(InvalidMetadataFieldTypeError):
-            validate_metadata_for_update({UnifiedMetadataKey.ISRC: ["USRC17607839"]})
-
-    def test_isrc_none_is_allowed(self):
-        validate_metadata_for_update({UnifiedMetadataKey.ISRC: None})
-
-    def test_isrc_empty_string_is_allowed(self):
-        validate_metadata_for_update({UnifiedMetadataKey.ISRC: ""})
 
 
 @pytest.mark.unit
